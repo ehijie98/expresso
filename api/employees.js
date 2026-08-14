5b62a824945ebc7adaaf857f57b1228293d47972
Ehijie <eaghedo1@gmail.com>
2026-08-14 19:28:42 +0100

    POST /api/employees method functioning and passing tests

--- api/employees.js
+++ api/employees.js
@@ -14,3 +14,36 @@ employeeRouter.get('/', (req, res, next) => {
             }
         });
 });
+
+employeeRouter.post('/', (req, res, next) => {
+    const employee = req.body.employee;
+    const name = employee.name;
+    const position = employee.position;
+    const wage = employee.wage;
+    const is_current_employee = employee.is_current_employee === 0 ? 0 : 1;
+
+    if (!employee || !name || !position || !wage) {
+        return res.sendStatus(400);
+    } 
+
+    const sql = `INSERT INTO Employee (name, position, wage, is_current_employee) 
+                VALUES ($name, $position, $wage, $isCurrentEmployee)`;
+    const values = {
+        $name: name,
+        $position: position,
+        $wage: wage,
+        $isCurrentEmployee: is_current_employee
+    };
+
+    db.run(sql, values, function(err) {
+        if (err) {
+            next(err);
+        } else {
+            db.get(`SELECT * FROM Employee WHERE id = ${this.lastID}`,
+                (err, employee) => {
+                    res.status(201).json({employee: employee});
+                }
+            );
+        }
+    });
+});
