e9c4babc730b72ad9c7577e48b9632e9bcde4393
Ehijie <eaghedo1@gmail.com>
2026-08-26 23:12:56 +0100

    app.js updated to allow for frontend deployment

--- app.js
+++ app.js
@@ -11,7 +11,15 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan('dev'));
 
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname, { index: false }));
+;
 app.use('/api', apiRouter);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.use(errorHandler());
 
--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const app = require('./app')
 const PORT = process.env.PORT || 4000;
 
 //checking server is running
-app.get('/', (req, res) => {
-  res.send('Server is running!');
-});
+// app.get('/', (req, res) => {
+//   res.send('Server is running!');
+// });
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
